e8b915742863a3a165775c8910a08f2fafb34dd8
lstawowy <l.w.stawowy@gmail.com>
2019-11-25 01:36:04 +0100

    Install axios for REST.

--- secure-application-front/src/main.js
+++ secure-application-front/src/main.js
@@ -1,19 +1,22 @@
 import Vue from "vue"
 import "./plugins/vuetify"
 import App from "./App.vue"
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie"
 import router from "./router/"
 import store from "./store"
 import "./registerServiceWorker"
 import "roboto-fontface/css/roboto/roboto-fontface.css"
 import "font-awesome/css/font-awesome.css"
 import i18n from "./lang/lang"
+import axios from "axios"
 
 Vue.config.productionTip = false
+window.axios = require("axios")
 
 new Vue({
   router,
   store,
   i18n,
+  axios,
   render: h => h(App)
 }).$mount("#app")
