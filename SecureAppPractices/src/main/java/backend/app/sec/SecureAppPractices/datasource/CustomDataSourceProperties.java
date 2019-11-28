b8c7e55f37e642f11b3187640dbd00606c800a7f
axal25 <emevig@gmail.com>
2019-11-29 00:28:31 +0100

    configuration for localhost

--- SecureAppPractices/src/main/java/backend/app/sec/SecureAppPractices/datasource/CustomDataSourceProperties.java
+++ SecureAppPractices/src/main/java/backend/app/sec/SecureAppPractices/datasource/CustomDataSourceProperties.java
@@ -44,19 +44,21 @@ public class CustomDataSourceProperties {
     private static final String gcpPattern_GAE_SERIVCE = "default";
     private static final String gcpPattern_GAE_RUNTIME = "java11";
     /**
-     { "GAE_ENV": "standard" },                                                                 // 3/3
-     { "GAE_SERVICE": "default" },                                                              // 3/3
-     { "GAE_RUNTIME": "java11" },                                                               // 3/3
-     { "GAE_APPLICATION": "e~braided-tracker-259922" },                                         // 2/3
-     { "GOOGLE_CLOUD_PROJECT": "braided-tracker-259922" },                                      // 2/3
-     { "GAE_VERSION": "springboot-helloworld" }, // dependant on to pom.xml -> not constant     // ?/?
+     { "GAE_ENV": "standard" },                                                                 // 5/5
+     { "GAE_SERVICE": "default" },                                                              // 5/5
+     { "GAE_RUNTIME": "java11" },                                                               // 5/5
+     { "GAE_APPLICATION": "e~braided-tracker-259922" },                                         // 4/5
+     { "GOOGLE_CLOUD_PROJECT": "braided-tracker-259922" },                                      // 4/5
+     { "GAE_VERSION": "secure-app-practices-version-from-pom-xml" }, // dependant on to pom.xml -> not constant
+     // use to be:
+     { "GAE_VERSION": "springboot-helloworld" },                                                // ?/?
      { "GAE_DEPLOYMENT_ID": "422777054906127631" }, // NOT CONSTANT                             // ?/?
      { "PORT": "8081" }, // NOT CONSTANT                                                        // ?/?
      **/
 
 
-//    private static final String target = "localhost";
-    private static final String databaseLocation = "gcp";
+    private static final String databaseLocation = "localhost";
+//    private static final String databaseLocation = "gcp";
     private String appDeploymentLocation = null;
 
     @Getter
