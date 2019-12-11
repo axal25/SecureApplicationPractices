51d5e2017de4062bc0721f3f66149f1d8cbb3fdb
axal25 <emevig@gmail.com>
2019-12-11 14:53:20 +0100

    changed PracticalCourseFragment class to abstract

--- AndroidSecureAppPractices/app/src/main/java/backend/app/secureapppractices/ui/main/courses/practical/PracticalCourseFragment.java
+++ AndroidSecureAppPractices/app/src/main/java/backend/app/secureapppractices/ui/main/courses/practical/PracticalCourseFragment.java
@@ -2,7 +2,7 @@ package backend.app.secureapppractices.ui.main.courses.practical;
 
 import backend.app.secureapppractices.ui.main.courses.CourseFragment;
 
-public class PracticalCourseFragment extends CourseFragment {
+public abstract class PracticalCourseFragment extends CourseFragment {
     public PracticalCourseFragment(int layout_id) {
         super(layout_id);
     }
